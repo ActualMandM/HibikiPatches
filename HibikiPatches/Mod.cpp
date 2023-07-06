10ca6d76a76f3ecdf763278a7455851ae77209a5
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-07-06 09:02:04 -0700

    Update hint addresses for Update 5.

--- HibikiPatches/Mod.cpp
+++ HibikiPatches/Mod.cpp
@@ -3,7 +3,7 @@
 SIG_SCAN
 (
 	sigUTOCSignature,
-	0x1424ADC66,
+	0x1425CF036,
 	"\x0F\x85\x00\x00\x00\x00\x40\x80\xE6\x04",
 	"xx????xxxx"
 );
@@ -11,7 +11,7 @@ SIG_SCAN
 SIG_SCAN
 (
 	sigPillarbox,
-	0x143F31755,
+	0x1440522D5,
 	"\xF6\x41\x30\x01\x45\x0F\x29\x43\x00",
 	"xxxxxxxx?"
 );
@@ -29,6 +29,6 @@ void Mod::init()
 	}
 	else
 	{
-		MessageBox(nullptr, TEXT("Failed to initialize! Please make sure that Hi-Fi RUSH is on Update 4 and try again."), TEXT("HibikiPatches"), MB_ICONERROR);
+		MessageBox(nullptr, TEXT("Failed to initialize! Please make sure that Hi-Fi RUSH is on Update 5 and try again."), TEXT("HibikiPatches"), MB_ICONERROR);
 	}
 }
